655363a0eaff0309ae1d2747e390ef6b4e53fc4e
fanyang <fanyang.coder@gmail.com>
2019-03-12 11:29:27 +0800

    try commit even get last block err

--- force-relay/side/handler.go
+++ force-relay/side/handler.go
@@ -13,13 +13,13 @@ func HandSideBlock(block *chainhandler.Block, actions []chainhandler.Action) {
 	blockCommitLast, err := GetLastCommittedBlock()
 	if err != nil {
 		seelog.Errorf("get last commit block err by %v", err.Error())
-		return
-	}
-	lastNum := blockCommitLast.GetNum()
-	num := block.GetNum()
-	if blockCommitLast != nil && num != 0 && lastNum >= num {
-		seelog.Debugf("no need commit %v to %v", lastNum, num)
-		return
+	} else {
+		lastNum := blockCommitLast.GetNum()
+		num := block.GetNum()
+		if blockCommitLast != nil && num != 0 && lastNum >= num {
+			seelog.Debugf("no need commit %v to %v", lastNum, num)
+			return
+		}
 	}
 
 	commitAct := newCommitAction(block, actions)
